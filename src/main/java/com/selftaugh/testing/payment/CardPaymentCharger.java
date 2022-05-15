d5731f8fcfe5e16df579fc499e5bffc71f84ae91
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2022-05-15 17:45:13 +0200

    Payment feature

---
+++ src/main/java/com/selftaugh/testing/payment/CardPaymentCharger.java
@@ -0,0 +1,13 @@
+package com.selftaugh.testing.payment;
+
+import java.math.BigDecimal;
+
+public interface CardPaymentCharger {
+
+    CardPaymentCharge chargeCard(
+            String cardSource,
+            BigDecimal amount,
+            Currency currency,
+            String description
+    );
+}
